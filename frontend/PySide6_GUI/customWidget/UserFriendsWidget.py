cd539c7c8985655311341f13016c0aa60f17fdf2
Cojocariu Daniel <cojocariudaniel02@gmail.com>
2025-04-29 19:09:28 +0300

    Cleaned up some files

--- frontend/PySide6_GUI/customWidget/UserFriendsWidget.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtCore import QTimer
-from PySide6.QtWidgets import QFrame, QWidget
-
-
-class LoadingBar(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-
